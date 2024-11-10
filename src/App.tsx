cc4c405553a6789981161a9defc21a273f0fd1bb
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2024-11-09 16:18:25 -0800

    feat: add surveyjs model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
-function App() {
-  return <main>App</main>;
-}
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.css";
+
+import json from "./data/survey.json";
+import { themeJson } from "./theme";
+
+const onComplete = (survey: { data: unknown }) => {
+  console.log("Survey complete! Results: " + JSON.stringify(survey.data));
+};
+
+const App = () => {
+  const survey = new Model(json);
+  survey.applyTheme(themeJson);
+
+  return (
+    <main>
+      <Survey model={survey} onComplete={onComplete} />
+    </main>
+  );
+};
 
 export default App;
